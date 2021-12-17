71ac6b644404194a0b0fa95215eb4732cd899e3d
meduduna Akerejola <cremmybanj7@gmail.com>
2021-12-17 14:08:23 +0100

    adding frontend

---
+++ frontend/src/_components/home/bossButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const BossButton = () => {
+	return(
+		<Button variant = 'contained' color = 'primary'>
+			BOSS
+		</Button>
+	);
+}
+
+export default BossButton;
