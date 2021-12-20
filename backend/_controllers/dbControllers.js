49702673be7a2b04c1767be36521caf0385053ec
meduduna Akerejola <cremmybanj7@gmail.com>
2021-12-20 11:48:31 +0100

    making processes asynchronous

--- backend/_controllers/dbControllers.js
+++ backend/_controllers/dbControllers.js
@@ -2,32 +2,49 @@ import Boss from '../_models/boss.js';
 import Worker from '../_models/worker.js';
 
 export const newBoss =  async (req,res)=>{
-	const { name, password, location } = req.body;
-	const newBoss = await new Boss({
-		name,
-		password,
-		location
-	});
-	const savedBoss = await newBoss.save();
-	res.json(`Your userId is ${savedBoss._id}`);
+	try{
+		const { name, password, location } = req.body;
+		const newBoss = await new Boss({
+			name,
+			password,
+			location
+		});
+		const savedBoss = await newBoss.save();
+		res.json(`Your userId is ${savedBoss._id}`);
+	}catch(err){
+		res.json('not successful');
+	}
 }
 
 export const newWorker =  async (req,res)=>{
-	const { name, password, location, bossID, skillset } = req.body;
-	const newWorker = await new Worker({
-		name,
-		password,
-		location,
-		bossID,
-		skillset
-	});
-	const savedWorker = await newWorker.save();
-	res.json(`Your userId is ${savedWorker._id}`);
+	try{
+		const { name, password, location, bossID, skillset } = req.body;
+		const newWorker = await new Worker({
+			name,
+			password,
+			location,
+			bossID,
+			skillset
+		});
+		const savedWorker = await newWorker.save();
+		res.json(`Your userId is ${savedWorker._id}`);
+	}catch(err){
+		res.json('not successful');
+	}
 }
 export const loginFunc = async (req,res) => {
-	const { bossID, password } = req.body;
-	const bossData = await Boss.findById({_id:bossID});
-	const pass = await bossData.password
-	// ( password === pass )?res.json('logged in successfully'):res.json('username and password are incorrect')
-	console.log(pass)
+	try{
+		const { bossID, password } = req.body;
+		try{
+			const bossData = await Boss.findById(bossID);
+			const idPassword = bossData.password;
+			const correct =  (password === idPassword)
+			correct ? res.json('logged in successfully'):res.json('username and password are incorrect')
+		}catch(err){
+			res.json('invalid ID')}
+		
+		// console.log(pass)
+	}catch(err){
+		console.log(err)
+	}
 }
