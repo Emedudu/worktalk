46978c9dcafcb71c4744e2a33d06e8bdf74a311d
meduduna Akerejola <cremmybanj7@gmail.com>
2021-12-21 11:37:04 +0100

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomeScreen from './_screens/HomeScreen.js';
 import BossLogin from './_screens/bossLogin.js';
 import WorkerLogin from './_screens/workerLogin.js';
@@ -8,6 +8,9 @@ import SuccessLogin from './_screens/successLogin'
 
 function App() {
 	const [isLogin, setIsLogin] = useState(false)
+	useEffect(()=>{
+		console.log(isLogin)
+	}, [isLogin])
 	return (
 	  	<Router>
 		    <div className="App">
@@ -15,10 +18,7 @@ function App() {
 				    <Route path = "/" exact element = { <HomeScreen /> } />
 				    <Route path = "/bossLogin" exact element = { <BossLogin isLogin = {isLogin} setIsLogin={setIsLogin}/> } />
 				    <Route path = "/workerLogin" exact element = { <WorkerLogin/> } />
-				    <Route path = "/successLogin" exact element = { () => { 
-				    	isLogin ? 
-				    	<SuccessLogin/>
-				    	: <HomeScreen/> }} />
+				    <Route path = "/successLogin" exact element = { isLogin ? <SuccessLogin /> : <Navigate to = '/' /> } />
 				</Routes>
 			</div>
 		</Router>
