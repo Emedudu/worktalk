8a283c030df0ae1d718cac9c1b9204a0b9dd9580
meduduna Akerejola <cremmybanj7@gmail.com>
2021-12-21 06:32:34 +0100

    implementing protected routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './_screens/HomeScreen.js';
 import BossLogin from './_screens/bossLogin.js';
 import WorkerLogin from './_screens/workerLogin.js';
+import SuccessLogin from './_screens/successLogin'
 
 function App() {
-  return (
-  	<Router>
-	    <div className="App">
-		    <Routes>
-			    <Route path = "/" exact element = { <HomeScreen/> } />
-			    <Route path = "/bossLogin" exact element = { <BossLogin/> } />
-			    <Route path = "/workerLogin" exact element = { <WorkerLogin/> } />
-			</Routes>
-		</div>
-	</Router>
-  );
+	const [isLogin, setIsLogin] = useState(false)
+	return (
+	  	<Router>
+		    <div className="App">
+			    <Routes>
+				    <Route path = "/" exact element = { <HomeScreen /> } />
+				    <Route path = "/bossLogin" exact element = { <BossLogin isLogin = {isLogin} setIsLogin={setIsLogin}/> } />
+				    <Route path = "/workerLogin" exact element = { <WorkerLogin/> } />
+				    <Route path = "/successLogin" exact element = { () => { 
+				    	isLogin ? 
+				    	<SuccessLogin/>
+				    	: <HomeScreen/> }} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
