0aa5c36ebf8f60d13ae3a650be542f7d319650a7
meduduna Akerejola <cremmybanj7@gmail.com>
2021-12-24 13:49:57 +0100

    retrying another way

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Routes, useHistory, Route, Navigate } from 'react-router-dom';
 import HomeScreen from './_screens/HomeScreen.js';
 import BossLogin from './_screens/bossLogin.js';
 import WorkerLogin from './_screens/workerLogin.js';
-import SuccessLogin from './_screens/successLogin'
+import SuccessLogin from './_screens/successLogin';
 
-export const history = createBrowserHistory();
-
+const isLogin = true;
 function App() {
-	let isLogin
-	const loginInfo = localStorage.getItem(isLogin)
-	loginInfo ? isLogin = loginInfo : localStorage.setItem(isLogin,false)
 	return (
-	  	<Router history = {history}>
+	  	<Router>
 		    <div className="App">
 			    <Routes>
 				    <Route path = "/" exact element = { <HomeScreen /> } />
