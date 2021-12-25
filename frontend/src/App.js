8322b4c50a29576a2653e0858f52329b50bd006c
meduduna Akerejola <cremmybanj7@gmail.com>
2021-12-25 14:42:27 +0100

    ...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, useHistory, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import HomeScreen from './_screens/HomeScreen.js';
 import BossLogin from './_screens/bossLogin.js';
 import WorkerLogin from './_screens/workerLogin.js';
 import SuccessLogin from './_screens/successLogin';
+import WorkerSuccessLogin from './_screens/workerSuccessLogin';
 
-const isLogin = true;
 function App() {
+	
 	return (
 	  	<Router>
 		    <div className="App">
@@ -15,7 +16,9 @@ function App() {
 				    <Route path = "/" exact element = { <HomeScreen /> } />
 				    <Route path = "/bossLogin" exact element = { <BossLogin /> } />
 				    <Route path = "/workerLogin" exact element = { <WorkerLogin/> } />
-				    <Route path = "/successLogin" exact element = { isLogin ? <SuccessLogin /> : <Navigate to = '/' /> } />
+				    <Route path = "/successLogin" exact element = { <SuccessLogin /> } />
+				    <Route path = "/workerSuccessLogin" exact element = { <WorkerSuccessLogin /> } />
+
 				</Routes>
 			</div>
 		</Router>
