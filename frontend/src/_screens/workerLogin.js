c87ad910698e3475074a6704a6e3f31402651e92
meduduna Akerejola <cremmybanj7@gmail.com>
2021-12-25 14:43:10 +0100

    ...

--- frontend/src/_screens/workerLogin.js
+++ frontend/src/_screens/workerLogin.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const WorkerLogin = () => {
-	const handleSubmit = (event) => {
+	const [workerID, setWorkerId] = useState('');
+	const [password, setPassword] = useState('');
+	const [bossID, setBossID] = useState('');
+	const navigate = useNavigate();
+
+	const handleSubmit = async(event) => {
 		event.preventDefault();
+		const config = {
+			headers : {
+				"Content-Type":"application/json"
+			}
+		}
 		try{
+			const res = await axios.post('/worker/login',{workerID, password, bossID},config);
+			res.data === 'true'?
+				navigate('/workerSuccessLogin'):
+					navigate('/')
+
 
 		}catch(err){console.log('an error occurred')}
 	}
 	return(
 		<Paper>
-			<form onSubmit = {handleSubmit} method = 'get'>
+			<form onSubmit = {handleSubmit}>
 				<TextField 
 				variant = 'outlined'
 				defaultValue = 'WORKER ID'
 				label = 'Worker Id'
+				onInput = {(e)=>{setWorkerId(e.target.value)}}
 				required
 				/>
 				<TextField 
@@ -25,12 +42,14 @@ const WorkerLogin = () => {
 				defaultValue = 'PASSWORD'
 				label = 'Password'
 				type = 'password'
+				onInput = {(e)=>{setPassword(e.target.value)}}
 				required
 				/>
 				<TextField 
 				variant = 'outlined'
 				defaultValue = 'BOSS ID'
 				label = 'Boss Id'
+				onInput = {(e)=>{setBossID(e.target.value)}}
 				required
 				/>
 				<Button variant = 'contained' color = 'primary' type = 'submit'>
