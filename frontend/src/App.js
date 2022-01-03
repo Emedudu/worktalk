15d442cbc1df0b14a8ea71364c708a47746bec32
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-03 15:58:45 +0100

    tidying up a little

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomeScreen from './_screens/HomeScreen.js';
 import BossLogin from './_screens/bossLogin.js';
 import WorkerLogin from './_screens/workerLogin.js';
-import SuccessLogin from './_screens/successLogin.js';
-import WorkerSuccessLogin from './_screens/workerSuccessLogin.js';
 import CreateAccountWorker from './_screens/createAccountWorker.js';
 
 function App() {
@@ -17,8 +15,6 @@ function App() {
 				    <Route path = "/" exact element = { <HomeScreen /> } />
 				    <Route path = "/bossLogin" exact element = { <BossLogin /> } />
 				    <Route path = "/workerLogin" exact element = { <WorkerLogin/> } />
-				    <Route path = "/successLogin" exact element = { <SuccessLogin /> } />
-				    <Route path = "/workerSuccessLogin" exact element = { <WorkerSuccessLogin /> } />
 				    <Route path = "/createAccountWorker" exact element = { <CreateAccountWorker /> }/>
 				</Routes>
 			</div>
