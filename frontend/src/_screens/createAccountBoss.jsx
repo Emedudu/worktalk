adf5261c246c5bd967486a3bc25bfc360ff9236a
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-03 19:06:38 +0100

    createAccountBoss

--- frontend/src/_screens/createAccountBoss.jsx
+++ frontend/src/_screens/createAccountBoss.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { Button, Typography } from '@material-ui/core';
+import axios from 'axios';
 
 const CreateAccountBoss = () => {
+    const [name,setName] = useState('');
+    const [password,setPassword] = useState('');
+    const [location,setLocation] = useState('');
+    const [response,setResponse] = useState('');
+    const handleSubmit = async(e)=>{
+        e.preventDefault();
+        const config = {
+			headers : {
+				"Content-Type":"application/json"
+			}
+		}
+        try{
+            const res = await axios.post("http://localhost:8001/boss/post",
+                {name, password, location},
+                config
+            )
+            setResponse(res.data)
+        }catch(err){console.log(err)}
+    }
+
+    const getLocation = () => {
+        const location = navigator.geolocation.getCurrentPosition(position => setLocation(`[${position.coords.latitude},${position.coords.longitude}]`));
+		location!==undefined ? 
+			setLocation(location):
+				setLocation('undefined');
+    }
     return(
-        <>
-        
-        </>
+        <form onSubmit = {handleSubmit}>
+            <TextField 
+            variant = 'outlined' 
+            label = 'Name'
+            onChange = { (e) => {setName(e.target.value)} }
+            type = 'text'
+            required
+            />
+            <TextField 
+            variant = 'outlined' 
+            label = 'Password'
+            onChange = { (e) => {setPassword(e.target.value)} }
+            type = 'password'
+            required
+            />
+            <TextField 
+            variant = 'outlined' 
+            label = 'Location'
+            onFocus = {getLocation}
+            />
+            <Button 
+            variant = 'contained' 
+            color = 'primary' 
+            type = 'submit'
+            >
+                CREATE ACCOUNT
+            </Button>
+            <Typography variant = 'body2'>
+                {response}
+            </Typography>
+            <Button 
+            variant = 'contained' 
+            color = 'primary' 
+            >
+                CONTINUE
+            </Button>
+        </form>
     );
 }
 
