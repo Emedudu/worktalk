4a936183723a80748a3bab62c2ffc4baf9ca82d8
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-05 15:01:30 +0100

    implementing websocket

--- frontend/src/_screens/mainScreenBoss.jsx
+++ frontend/src/_screens/mainScreenBoss.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 
 const MainScreenBoss = ()=>{
+    axios.get('http://localhost:8001/boss/chat')
+        .then( res => {
+            const ws = new WebSocket('ws://localhost:8009')
+        }
+    )
     return(
         <>
         
