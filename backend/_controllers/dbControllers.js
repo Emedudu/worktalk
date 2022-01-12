75d8b2160d20930a86985bf16d58f0de620f73c8
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-12 10:49:37 +0100

    implementing ws

--- backend/_controllers/dbControllers.js
+++ backend/_controllers/dbControllers.js
@@ -1,6 +1,6 @@
 import Boss from '../_models/boss.js';
 import Worker from '../_models/worker.js';
-import { WebSocketServer } from 'ws' ;
+import WebSocket, { WebSocketServer } from 'ws' ;
 
 export const newBoss =  async (req,res)=>{
 	try{
@@ -68,18 +68,5 @@ export const loginFuncWorker = async(req,res) => {
 			res.json(false)}
 	}catch(err){
 		console.log(err)
-	}
-}
-export const chatFunc = async(req,res)=>{
-	try{
-		const wss = new WebSocketServer({port:8009});
-		wss.on("connection",ws=>{
-			console.log('New socket connected');
-			ws.on("close",()=>{
-				console.log('client disconnected');
-			})
-		})
-	}catch(err){
-		console.log('err');
 	}
 }
