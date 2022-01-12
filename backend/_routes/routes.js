5a0c2d45ea9a8099255cc9a8c1b03948f682320d
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-12 10:50:02 +0100

    ...

--- backend/_routes/routes.js
+++ backend/_routes/routes.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { newBoss, newWorker, loginFunc, loginFuncWorker, chatFunc } from '../_controllers/dbControllers.js';
+import { newBoss, newWorker, loginFunc, loginFuncWorker } from '../_controllers/dbControllers.js';
 
 export const bossRouter = express.Router();
 
 bossRouter.route('/post').post(newBoss);
 bossRouter.route('/login').post(loginFunc);
-bossRouter.route('/chat').get(chatFunc);
 
 export const workerRouter = express.Router();
 
