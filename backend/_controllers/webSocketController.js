ff7695a623424d7f744a8dc4e8e256b0ba904c96
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-14 19:03:05 +0100

    websocketcontroller

--- backend/_controllers/webSocketController.js
+++ backend/_controllers/webSocketController.js
@@ -1,4 +1,5 @@
 import { WebSocketServer } from 'ws' ;
+import BossOrder from '../_models/bossToWorker.js';
 
 const websocketServer = new WebSocketServer({
   noServer: true
@@ -11,7 +12,18 @@ export const connectServer = (server)=>{
       });
     }
 websocketServer.on('connection',(ws)=>{
-  ws.send('hey');
-  ws.on('message',(message)=>ws.send(message.toString()))
+  ws.on('message',async(order)=>{
+    const frontendForm = JSON.parse(order);
+    const skillset = frontendForm.skillset.toString();
+    const location = frontendForm.location.toString();
+    const message = frontendForm.message.toString();
+    const timestamp = frontendForm.date.toString();
+    const to = frontendForm.message.toString();
+    const from = frontendForm.from.toString();
+    try{
+      const newBossOrder = await new BossOrder({message,timestamp,to,from})
+      newBossOrder.save()
+    }catch(err){console.log(err)}
+  })
   ws.on('close',()=>console.log('disconnected successfully'))
 })
