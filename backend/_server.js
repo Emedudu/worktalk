464fa2590ec9f05429996285574adb51864435d8
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-14 11:07:31 +0100

    refactoring ws

--- backend/_server.js
+++ backend/_server.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './connectdb.js';
 import { bossRouter, workerRouter } from './_routes/routes.js';
-import WebSocket, { WebSocketServer } from 'ws' ;
+import { WebSocketServer } from 'ws' ;
+import { connectServer } from './_controllers/webSocketController.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
@@ -17,16 +18,5 @@ app.use('/worker',workerRouter);
 
 connectDB();
 
-const websocketServer = new WebSocketServer({
-    noServer: true
-  });
-app.listen(PORT,console.log(`server is running on ${PORT}`)).
-    on('upgrade',(request, socket, head) => {
-        websocketServer.handleUpgrade(request, socket, head, (websocket) => {
-          websocketServer.emit("connection", websocket, request);
-        });
-      });
-websocketServer.on('connection',(ws)=>{
-    ws.on('message',(message)=>console.log(message.toString()))
-    ws.on('close',()=>console.log('disconnected successfully'))
-})
+
+connectServer(app.listen(PORT,console.log(`server is running on ${PORT}`)))
