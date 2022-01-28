bc83c8c75d6f5fdd2a6b7c73536b39a670db4c43
meduduna Akerejola <cremmybanj7@gmail.com>
2022-01-28 16:55:50 +0100

    ...

--- backend/_controllers/orderProcessor.js
+++ backend/_controllers/orderProcessor.js
@@ -2,8 +2,9 @@ import Worker from "../_models/worker.js"
 
 export const ProcessOrder = async(fromId,skillset,location)=>{
     try{
-        const allWorkers = await Worker.findById(fromId);
-        return allWorkers.skillset
+        const allWorkers = await Worker.find({ bossID: fromId });
+        console.log(skillset);
+        return fromId;
     }catch(err){
         console.log('Error from orderProcessor.js')
     }
